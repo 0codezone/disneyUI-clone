648f710581e022492a0c43500174bafd700c0c34
0codezone <mohittailor997@gmail.com>
2024-03-10 01:24:46 +0530

    use temprory login functionality  anduse react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import Header from "./components/Header";
-import Slider from "./components/Slider";
-import Productionhouse from "./components/Productionhouse";
-import GenreMovieList from "./components/GenreMovieList";
+import HomePage from "./HomePage"; // Make sure this path is correct
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    if (username === "user" && password === "password") {
+      setLoggedIn(true);
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
-      <Header />
-      <Slider />
-      <Productionhouse />
-      <GenreMovieList />
+      <Routes>
+        {/* Use anonymous function to delay execution of handleLogin */}
+        <Route
+          path="/"
+          element={
+            loggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/home" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            loggedIn ? (
+              <Navigate to="/home" />
+            ) : (
+              <div className=" flex justify-center items-center w-full h-[100vh]">
+                <div className="flex flex-col gap-5">
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="rounded-md border-none outline-none p-2 text-black"
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="rounded-md border-none outline-none p-2  text-black"
+                  />
+                  <button
+                    onClick={handleLoginClick}
+                    className="p-2 bg-blue-500 rounded-md"
+                  >
+                    Login
+                  </button>
+
+                  <p className="text-red-500">
+                    Login using <br />
+                    <span>username : user , password : password</span>
+                  </p>
+                </div>
+              </div>
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 };
---
+++ src/HomePage.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import Productionhouse from "./components/Productionhouse";
+import GenreMovieList from "./components/GenreMovieList";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <Productionhouse />
+      <GenreMovieList />
+    </div>
+  );
+};
+
+export default HomePage;
